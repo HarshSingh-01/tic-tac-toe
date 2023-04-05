5dfa8d6de6eec16a61edbebafc3283795ee36b0d
Harsh Singh <harshkushwah2011@gmail.com>
2023-04-05 15:31:44 +0000

    some change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <p>I am really happy with it's funcionality. Awesome!!!</p>
+      <p>I am really happy with it's funcionality. Awesome!!! Pretty!!!
+      </p>
     </div>
   );
 }
